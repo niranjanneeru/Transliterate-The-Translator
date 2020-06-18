8af9d735354f22227616bf4a5504cc9a18871309
Niranjan B <niranjannb7777@gmail.com>
2020-06-18 23:37:42 +0530

    requirements.txt

--- Transliterateapp/settings.py
+++ Transliterateapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6-$fl6^_-gbcdpgqc%eoq9m21pgck89g%w)^o$s%)ies)j3aj&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
